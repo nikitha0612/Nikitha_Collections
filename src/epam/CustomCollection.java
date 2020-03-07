b4b207fb35b2f7394c23088304c14de3902ad9a6
nikitha0612 <60382626+nikitha0612@users.noreply.github.com>
2020-03-07 07:35:10 +0530

    Add files via upload

---
+++ src/epam/CustomCollection.java
@@ -0,0 +1,10 @@
+package epam;
+
+public class CustomCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/epam/CustomList.java
@@ -0,0 +1,10 @@
+package epam;
+
+public class CustomList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
